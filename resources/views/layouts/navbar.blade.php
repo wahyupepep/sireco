411a2994d7d866157302ae178c0e08d9896e9b09
abdurahmanrizal1@gmail.com <abdurahmanrizal1@gmail.com>
2022-07-01 00:06:30 +0700

    feat(): FE notifications

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -27,10 +27,67 @@
               <i class="mdi mdi-logout mr-2 text-primary"></i> Signout </a>
           </div>
         </li>
-        <li class="nav-item d-none d-lg-block full-screen-link">
-          <a class="nav-link">
-            <i class="mdi mdi-bell" id="bell-button"></i>
+        {{-- <li class="nav-item d-none d-lg-block full-screen-link"> --}}
+        {{-- <li class="nav-item nav-profile dropdown dropdown-notification">
+          <a class="nav-link dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+            <div class="nav-profile-text">
+              <i class="mdi mdi-bell" id="bell-button"></i><sup style="color: red" class="rounded-circle">1</sup>
+            </div>
+           
           </a>
+          <div class="dropdown-menu navbar-dropdown" aria-labelledby="NotificationDropdown">
+            <a class="dropdown-item" href="#">
+              <i class="mdi mdi-cached mr-2 text-success">Hello World</i> 
+             </a>
+            <div class="dropdown-divider"></div>
+          </div>
+        </li> --}}
+        <li class="nav-item dropdown">
+          <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+            <i class="mdi mdi-bell-outline"></i>
+            <span class="count-symbol bg-danger"></span>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown" style="width: 300px">
+            <h6 class="p-3 mb-0">Notifications</h6>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item preview-item">
+              <div class="preview-thumbnail">
+                <div class="preview-icon bg-success">
+                  <i class="mdi mdi-cart-plus"></i>
+                </div>
+              </div>
+              <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                <h6 class="preview-subject font-weight-normal mb-1 font-weight-bold">New Booking</h6>
+                <p class="text-gray ellipsis mb-0 text-muted" style="font-size: 13px"> INV-2022/07/01/A1 </p>
+              </div>
+            </a>
+            {{-- <div class="dropdown-divider"></div>
+            <a class="dropdown-item preview-item">
+              <div class="preview-thumbnail">
+                <div class="preview-icon bg-warning">
+                  <i class="mdi mdi-settings"></i>
+                </div>
+              </div>
+              <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                <h6 class="preview-subject font-weight-normal mb-1">Settings</h6>
+                <p class="text-gray ellipsis mb-0"> Update dashboard </p>
+              </div>
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item preview-item">
+              <div class="preview-thumbnail">
+                <div class="preview-icon bg-info">
+                  <i class="mdi mdi-link-variant"></i>
+                </div>
+              </div>
+              <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
+                <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
+                <p class="text-gray ellipsis mb-0"> New admin wow! </p>
+              </div>
+            </a>
+            <div class="dropdown-divider"></div>
+            <h6 class="p-3 mb-0 text-center">See all notifications</h6> --}}
+          </div>
         </li>
        
         <li class="nav-item nav-logout d-none d-lg-block">
