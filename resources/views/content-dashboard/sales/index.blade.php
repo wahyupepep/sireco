6cf161d408a2dd76d4e117a23db6e4807ecbe9d8
wahyupepep <wahyufebri69@gmail.com>
2022-07-04 17:27:57 +0700

    sales index

--- resources/views/content-dashboard/sales/index.blade.php
+++ resources/views/content-dashboard/sales/index.blade.php
@@ -10,7 +10,22 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                
+                @include('layouts.message')
+                <table class="table table-bordered data-table">
+                    <thead>
+                        <tr>
+                            <th>No</th> 
+                            <th>Invoice Number</th>
+                            <th>Member Name</th>
+                            <th>Duration</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
